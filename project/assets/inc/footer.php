96ac4f9fe49d9e8374ad4c80a51959e8db79e5a1
alisashehaj <128844487+alisashehaj@users.noreply.github.com>
2023-06-19 22:33:45 +0200

    Add files via upload

---
+++ project/assets/inc/footer.php
@@ -0,0 +1,4 @@
+<div class="splash-footer">
+  &copy; <?php echo date('Y'); ?>
+  Online Railway Reservation System | Developed By <a href="https://sixhei-tartari.vercel.app/" target="_blank">Sixhei Tartari</a>
+</div>
